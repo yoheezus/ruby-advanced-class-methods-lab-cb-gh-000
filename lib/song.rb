1cef955c887f135739444f1dc7a7be14c6e79b1c
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-12 13:27:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name, artist_name)
+  def create(name, artist_name)
       song = self.new
       song.name = name
       song.artist_name = artist_name
