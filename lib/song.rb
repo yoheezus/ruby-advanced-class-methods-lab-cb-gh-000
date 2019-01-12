c6507ca05b88cc73186999a4bb5b1667c72f120a
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-12 15:02:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,8 @@ class Song
       self.all.find{ |song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+      self.find_by_name(name) ? self.find_by_name : self.create_by_name(name)
+  end
+
 end
